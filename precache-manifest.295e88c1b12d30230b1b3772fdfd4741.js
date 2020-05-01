6c27ef771e10cf536d2abdcbace553b620d073f6
unknown <bmalkes@gmail.com>
2020-05-01 14:01:52 +0300

    Updates

---
+++ precache-manifest.295e88c1b12d30230b1b3772fdfd4741.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd066a7a51d950a0f6191875d9c9892d",
+    "url": "/Clone-Netflix-React/index.html"
+  },
+  {
+    "revision": "d8305b8319cf963bd3db",
+    "url": "/Clone-Netflix-React/static/css/main.f64d96fc.chunk.css"
+  },
+  {
+    "revision": "768aef83de9796b9a35e",
+    "url": "/Clone-Netflix-React/static/js/2.bff06362.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Clone-Netflix-React/static/js/2.bff06362.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8305b8319cf963bd3db",
+    "url": "/Clone-Netflix-React/static/js/main.07d28b5a.chunk.js"
+  },
+  {
+    "revision": "eded1422827f46e9793a",
+    "url": "/Clone-Netflix-React/static/js/runtime-main.762cc991.js"
+  }
+]);
